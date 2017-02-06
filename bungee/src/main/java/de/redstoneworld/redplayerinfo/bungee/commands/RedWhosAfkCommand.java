615fca8e7610ccfd7381a4564d693b021a53fa2d
Max Lee <mail@moep.tv>
2017-02-06 18:26:55 +0100

    Implement whosafk command

--- bungee/src/main/java/de/redstoneworld/redplayerinfo/bungee/commands/RedWhosAfkCommand.java
+++ bungee/src/main/java/de/redstoneworld/redplayerinfo/bungee/commands/RedWhosAfkCommand.java
@@ -1,8 +1,14 @@
 package de.redstoneworld.redplayerinfo.bungee.commands;
 
+import de.redstoneworld.redplayerinfo.bungee.RedPlayer;
+import de.redstoneworld.redplayerinfo.bungee.RedPlayerInfo;
 import de.themoep.bungeeplugin.BungeePlugin;
 import de.themoep.bungeeplugin.PluginCommand;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RedWhosAfkCommand extends PluginCommand {
 
@@ -12,6 +18,20 @@ public class RedWhosAfkCommand extends PluginCommand {
 
     @Override
     public boolean run(CommandSender sender, String[] args) {
-        return false;
+        List<String> players = new ArrayList<>();
+        for (ProxiedPlayer player : plugin.getProxy().getPlayers()) {
+            RedPlayer redPlayer = ((RedPlayerInfo) plugin).getPlayer(player);
+            if (redPlayer.isAfk()) {
+                players.add(redPlayer.getName());
+            }
+        }
+
+        StringBuilder playersString = new StringBuilder(players.size() > 0 ? players.get(0) : "");
+        for(int i = 1; i < players.size(); i++) {
+            playersString.append(", ").append(players.get(i));
+        }
+
+        sender.sendMessage(plugin.translate(plugin.getConfig().getString("messages.whos-afk"), "players", playersString.toString()));
+        return true;
     }
 }
