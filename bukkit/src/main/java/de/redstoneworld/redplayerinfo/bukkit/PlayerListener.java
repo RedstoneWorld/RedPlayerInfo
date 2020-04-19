0e3d344181302f4d2d08c7cfadc3839273181879
Phoenix616 <mail@moep.tv>
2020-04-19 18:38:32 +0100

    Don't ignore cancelled events and add more to listen for (#3)

--- bukkit/src/main/java/de/redstoneworld/redplayerinfo/bukkit/PlayerListener.java
+++ bukkit/src/main/java/de/redstoneworld/redplayerinfo/bukkit/PlayerListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
@@ -19,6 +20,9 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
 
 public class PlayerListener implements Listener {
     private final RedPlayerInfoBukkit plugin;
@@ -37,81 +41,106 @@ public class PlayerListener implements Listener {
         plugin.updateLastActivity(event.getPlayer());
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onInventoryOpen(InventoryOpenEvent event) {
         if (event.getPlayer() instanceof Player) {
             plugin.updateLastActivity((Player) event.getPlayer());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         if (event.getPlayer() instanceof Player) {
             plugin.updateLastActivity((Player) event.getPlayer());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onInventoryInteract(InventoryClickEvent event) {
         if (event.getWhoClicked() instanceof Player) {
             plugin.updateLastActivity((Player) event.getWhoClicked());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
+    public void onInventoryInteract(InventoryDragEvent event) {
+        if (event.getWhoClicked() instanceof Player) {
+            plugin.updateLastActivity((Player) event.getWhoClicked());
+        }
+    }
+
+    @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         plugin.updateLastActivity(event.getPlayer());
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         plugin.updateLastActivity(event.getPlayer());
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         if (event.getAction() != Action.PHYSICAL) {
             plugin.updateLastActivity(event.getPlayer());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        if (event.getFrom().getBlock() != event.getTo().getBlock()) {
+        if (event.getFrom().getBlockX() != event.getTo().getBlockX()
+                || event.getFrom().getBlockZ() != event.getTo().getBlockZ()
+                || event.getFrom().getBlockY() != event.getTo().getBlockY()
+        ) {
             plugin.updateLastActivity(event.getPlayer());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
+    public void onSneak(PlayerToggleSneakEvent event) {
+        plugin.updateLastActivity(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onSprint(PlayerToggleSprintEvent event) {
+        plugin.updateLastActivity(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onFly(PlayerToggleFlightEvent event) {
+        plugin.updateLastActivity(event.getPlayer());
+    }
+
+    @EventHandler
     public void onTeleport(PlayerTeleportEvent event) {
         if (event.getFrom() != event.getTo()) {
             plugin.updateLastActivity(event.getPlayer());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onChangeWorld(PlayerChangedWorldEvent event) {
         plugin.updateLastActivity(event.getPlayer());
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onCommand(PlayerCommandPreprocessEvent event) {
         plugin.updateLastActivity(event.getPlayer());
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
         plugin.updateLastActivity(event.getPlayer());
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onDamage(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player) {
             plugin.updateLastActivity((Player) event.getEntity());
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onAttack(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player) {
             plugin.updateLastActivity((Player) event.getDamager());
